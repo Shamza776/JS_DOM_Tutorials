732f90fc39de583a0dd590c84a4e577aa9a124eb
Shamza <sharleenjamoza@gmail.com>
2024-01-05 21:13:54 +0300

    modified the three files ready for the DOM manipulation

--- readList.js
+++ readList.js
@@ -1,1 +1,17 @@
- 
+/*function cycleClasses (){
+    let elements = document.getElementsByClassName('title');
+    for(let i=0;i<elements.length;i++){
+        console.log(elements[i]);
+    } 
+    console.log(elements[1]);
+}   
+cycleClasses(); 
+
+//console.log(Array.isArray(elements)); //checks whether it's an array
+//console.log(Array.isArray(Array.from(elements))) // turn it into an array
+let elements = document.getElementsByClassName('title');
+ //turn the selected classes to an array, then use the forEach method for arrays to print them all out
+ Array.from(elements).forEach(function(item){    
+    console.log(item)
+}) */
+
