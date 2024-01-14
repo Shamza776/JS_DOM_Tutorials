ad5367bd77fa32249e2761507959db57b7defd8f
Shamza <sharleenjamoza@gmail.com>
2024-01-14 17:56:44 +0300

    added functionality on the delete button

--- readList.js
+++ readList.js
@@ -15,3 +15,22 @@ let elements = document.getElementsByClassName('title');
     console.log(item)
 }) */
 
+//we're adding functionality to the remove button
+//we grab the element
+ let btn = document.querySelectorAll('#book-list .delete')
+ console.log(btn);
+//change them to an array
+btn = Array.from(btn)
+console.log(btn)
+//add an array method to add more methods on the new array
+btn.forEach(function(btn){
+    btn.addEventListener('click', function(e){
+       // if(confirm("Do you want to delete this book?")){
+            e.target.parentNode.remove()
+        //}
+        //OR
+       //let parent = document.getElementById('#book-list');
+       //let li = e.target.parentElement
+       //li.parentNode.removeChild(li);
+    })
+})
